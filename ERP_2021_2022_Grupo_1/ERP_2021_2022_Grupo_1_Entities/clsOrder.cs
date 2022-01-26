2e55847c18a1646897952e56ac30182a4778c340
gfgffgf <daff@gmail.com>
2022-01-26 22:26:02 +0100

    Entities creadas, falta analisis de clases

--- ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_Entities/clsOrder.cs
+++ ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_Entities/clsOrder.cs
@@ -6,5 +6,27 @@ namespace ERP_2021_2022_Grupo_1_Entities
 {
     public class clsOrder
     {
+        int orderId;
+        int total;
+        DateTime orderDate;
+        DateTime limitOrderDate;
+        String notes;
+        clsSupplier suplier;
+
+        public clsOrder(int orderId, int total, DateTime orderDate, DateTime limitOrderDate, string notes, clsSupplier suplier)
+        {
+            this.orderId = orderId;
+            this.total = total;
+            this.orderDate = orderDate;
+            this.limitOrderDate = limitOrderDate;
+            this.notes = notes;
+        }
+
+        public int OrderId { get => orderId; }
+        public int Total { get => total; }
+        public DateTime OrderDate { get => orderDate;  }
+        public DateTime LimitOrderDate { get => limitOrderDate; }
+        public string Notes { get => notes; set => notes = value; }
+        public clsSupplier Suplier { get => suplier; set => suplier = value; }
     }
 }
--- ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_Entities/clsProduct.cs
+++ ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_Entities/clsProduct.cs
@@ -6,11 +6,26 @@ namespace ERP_2021_2022_Grupo_1_Entities
 {
     public class clsProduct
     {
-        int id;
+        int orderId;
         string name;
         string description;
         double unitPrice;
         string category;
+
+        public clsProduct(int orderId, string name, string description, double unitPrice, string category)
+        {
+            this.orderId = orderId;
+            this.name = name;
+            this.description = description;
+            this.unitPrice = unitPrice;
+            this.category = category;
+        }
+
+        public int OrderId { get => orderId; }
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public double UnitPrice { get => unitPrice; set => unitPrice = value; }
+        public string Category { get => category; set => category = value; }
 
     }
 }

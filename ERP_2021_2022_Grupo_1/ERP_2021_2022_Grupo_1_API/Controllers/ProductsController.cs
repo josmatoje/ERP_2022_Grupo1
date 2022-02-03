742310020ec503d97f6913c5caef2fdc2b366937
German <german.debustamante@iesnervion.es>
2022-02-03 20:44:23 +0100

    Controllers API Listos

---
+++ ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_API/Controllers/ProductsController.cs
@@ -0,0 +1,71 @@
+﻿using ERP_2021_2022_Grupo_1_BL.Lists;
+using ERP_2021_2022_Grupo_1_Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using FromBodyAttribute = Microsoft.AspNetCore.Mvc.FromBodyAttribute;
+using HttpDeleteAttribute = Microsoft.AspNetCore.Mvc.HttpDeleteAttribute;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+using HttpPutAttribute = Microsoft.AspNetCore.Mvc.HttpPutAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ERP_2021_2022_Grupo_1_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/<ProductsController>
+        //TODO COMENT
+        [HttpGet]
+        public IEnumerable<clsProduct> Get()
+        {
+            List<clsProduct> productList = null;
+            try
+            {
+                productList = new List<clsProduct>(clsProductsListBL.getProductsListBL());
+            }
+            catch (Exception e)
+            { }
+
+            return productList;
+        }
+
+        // GET api/<ProductsController>/5
+        //TODO COMENT
+        [HttpGet("{id}")]
+        public clsProduct Get(int id)
+        {
+            clsProduct product = null;
+            try
+            {
+                product = clsProductsListBL.getProductBL(id);
+            }
+            catch (Exception e) { }
+            return product;
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_API/Controllers/SuppliersController.cs
@@ -0,0 +1,70 @@
+﻿using ERP_2021_2022_Grupo_1_BL.Lists;
+using ERP_2021_2022_Grupo_1_BL.Manager;
+using ERP_2021_2022_Grupo_1_Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using FromBodyAttribute = Microsoft.AspNetCore.Mvc.FromBodyAttribute;
+using HttpDeleteAttribute = Microsoft.AspNetCore.Mvc.HttpDeleteAttribute;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+using HttpPutAttribute = Microsoft.AspNetCore.Mvc.HttpPutAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ERP_2021_2022_Grupo_1_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        // GET: api/<SuppliersController>
+        [HttpGet]
+        public IEnumerable<clsSupplier> Get()
+        {
+            List<clsSupplier> suppliersList = null;
+            try
+            {
+                suppliersList = new List<clsSupplier>(clsSuppliersListBL.getSuppliersListBL());
+            }
+            catch (Exception e)
+            { }
+
+            return suppliersList;
+        }
+
+        // GET api/<SuppliersController>/5
+        [HttpGet("{id}")]
+        public clsSupplier Get(int id)
+        {
+            clsSupplier supplier = null;
+            try
+            {
+                supplier = clsSuppliersListBL.getSupplierBL(id);
+            }
+            catch (Exception e) { }
+            return supplier;
+        }
+
+        // POST api/<SuppliersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SuppliersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SuppliersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

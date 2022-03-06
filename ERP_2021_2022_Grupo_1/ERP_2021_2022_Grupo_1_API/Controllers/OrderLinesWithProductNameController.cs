337aea78bb997c1e920ce121d03c53f2b3037fae
German <german.debustamante@iesnervion.es>
2022-03-06 20:01:58 +0100

    clsOrderLineWithProductName sin DAL

---
+++ ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_API/Controllers/OrderLinesWithProductNameController.cs
@@ -0,0 +1,30 @@
+﻿using ERP_2021_2022_Grupo_1_BL.Lists;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ERP_2021_2022_Grupo_1_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderLinesWithProductNameController : ControllerBase
+    {
+        // GET api/<OrderLinesWithProductNameController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            clsOrderLineWithProductName orderLineWithProductName = null;
+            try
+            {
+                orderLineWithProductName = new clsOrderLineWithProductNameListBL().geOrderLineWithProductNameBL(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return orderLineWithProductName;
+        }
+    }
+}
---
+++ ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_BL/Lists/clsOrderLineWithProductNameListBL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP_2021_2022_Grupo_1_BL.Lists
+{
+    public class clsOrderLineWithProductNameListBL
+    {
+        public clsOrderLineWithProductName geOrderLineWithProductNameBL(int id)
+        {
+            return new cls
+        }
+    }
+}

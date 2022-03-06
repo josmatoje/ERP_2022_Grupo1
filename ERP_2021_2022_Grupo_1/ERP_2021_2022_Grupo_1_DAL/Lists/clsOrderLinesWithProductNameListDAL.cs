b0ebe3ea7e0c3c71e2e37dacd2ccfc1bd048c9ad
josmatoje <josemariamataojeda@gmail.com>
2022-03-06 20:24:24 +0100

    Auto stash before merge of "Backend" and "origin/Backend"

--- ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_DAL/Lists/clsOrderLinesWithProductNameListDAL.cs
+++ ERP_2021_2022_Grupo_1/ERP_2021_2022_Grupo_1_DAL/Lists/clsOrderLinesWithProductNameListDAL.cs
@@ -10,7 +10,30 @@ namespace ERP_2021_2022_Grupo_1_DAL.Lists
     {
         public List<clsOrderLineWithProductName> getOrderLineListWithProductNameByOrderIdDAL(int id)
         {
-            return null;
+            clsOrderLineWithProductName orderLine;
+            List<clsOrderLineWithProductName> orderLineList = new List<clsOrderLineWithProductName>();
+            openConection();
+            MyReader = executeSelectCondition("SELECT [OrderLines].ID, Quantity, UnitPriceAtTime, OrderID, ProductID, Subtotal, Name FROM OrderLines " +
+                                        "INNER JOIN Products ON [OrderLines].ProductID = [Products].ID" +
+                                        "WHERE OrderId = @id", id);
+
+
+            if (MyReader.HasRows)
+            {
+                while (MyReader.Read())
+                {
+                    orderLine = new clsOrderLineWithProductName((int)MyReader["ID"],
+                                                    (int)MyReader["Quantity"],
+                                                    Decimal.ToDouble((decimal)MyReader["UnitPriceAtTime"]),
+                                                    Decimal.ToDouble((decimal)MyReader["UnitPriceAtTime"]),
+                                                    (int)MyReader["OrderID"],
+                                                    (int)MyReader["ProductID"],
+                                                    (string)MyReader["Name"]);
+                    orderLineList.Add(orderLine);
+                }
+            }
+            closeFlow();
+            return orderLineList;
         }
     }
 }
